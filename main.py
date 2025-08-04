86056ebcb2251fa485986553ae9565bd7ea6fb17
Sachin Kumar Mishra <165578484+SachinKrMishra@users.noreply.github.com>
2025-08-04 21:57:48 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
 if __name__ == '__main__':
     try :
-        trainingpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
-        data_ingestion = DataIngestion(dataingestionconfig)
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
         logging.info('Initiate Data Ingestion')
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info('Data Initiation Completed')
         print(data_ingestion_artifact)
+
+        datavalidationconfig = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, datavalidationconfig)
+        logging.info("Initiate Data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
